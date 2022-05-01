1e93bf090969c58d4da4e465398d2699622344a9
Toomas Park <zapacher>
2022-05-01 21:53:55 +0300

    Homework lesson1  task 12

---
+++ src/student_toomas_park/homework/lesson_1/task12.java
@@ -0,0 +1,9 @@
+package student_toomas_park.homework.lesson_1;
+
+public class task12 {
+    public static void main(String[] args) {
+        float num1 = 50, num2 = 3, res;
+        res = num1 * num2;
+        System.out.print (res);
+    }
+}
