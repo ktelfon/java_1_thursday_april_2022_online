6783f48a773268e00f8a55bb9296a1fbb4b04f58
Toomas Park <zapacher>
2022-05-01 22:05:34 +0300

    Homework lesson2  task 24

---
+++ src/student_toomas_park/homework/lesson_2/task24.java
@@ -0,0 +1,8 @@
+package student_toomas_park.homework.lesson_2;
+
+public class task24 {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
