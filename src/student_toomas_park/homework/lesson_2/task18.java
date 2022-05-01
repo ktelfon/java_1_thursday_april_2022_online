e2937fc9b3b8633332a1e32ce28ee8c6a2c66c05
Toomas Park <zapacher>
2022-05-01 21:57:49 +0300

    Homework lesson2  task 18 Проблема была в заглавной букве System

---
+++ src/student_toomas_park/homework/lesson_2/task18.java
@@ -0,0 +1,8 @@
+package student_toomas_park.homework.lesson_2;
+
+public class task18 {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
