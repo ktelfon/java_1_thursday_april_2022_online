a9ed831f13f393515c5dc12939e2e950affb8e0d
Toomas Park <zapacher>
2022-05-01 22:08:52 +0300

    Renamed the classes

--- src/student_toomas_park/homework/lesson_2/Task24.java
+++ src/student_toomas_park/homework/lesson_2/Task24.java
@@ -1,6 +1,6 @@
 package student_toomas_park.homework.lesson_2;
 
-public class task24 {
+public class Task24 {
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
         System.out.println("So bad");
