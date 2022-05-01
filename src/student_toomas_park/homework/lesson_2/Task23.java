58ca0d979d952aa835cbe6dadce93c138180066c
Toomas Park <zapacher>
2022-05-01 22:30:15 +0300

    homework lesson2 Task23

--- src/student_toomas_park/homework/lesson_2/Task23.java
+++ src/student_toomas_park/homework/lesson_2/Task23.java
@@ -1,7 +1,8 @@
 package student_toomas_park.homework.lesson_2;
 
 public class Task23 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
     System.out.println("Code outside the method will not compile!");
     }
 }
