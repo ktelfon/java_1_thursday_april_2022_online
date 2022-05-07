5f3ddf271f28ce998351c7f86da1262c4db0ced6
Artemij <artik8865@inbox.lv>
2022-05-07 17:41:03 +0300

    All in progress

---
+++ src/student_artemijs_talamajevs/homework/lesson_2_variables/homework/LoginService.java
@@ -0,0 +1,23 @@
+package student_artemijs_talamajevs.homework.lesson_2_variables.homework;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+    //Дан код, нужно исправить код main() метода так,
+    //чтобы программу можно было запустить.
+    //Функционал класса писать не надо.
+    //Для сдачи работы создайте исправленный класс.
+    //
+    //class LoginService {
+    //
+    //	public void main(String[] args) {
+    //		System.out.println("You can login!");
+    //	}
+    //
+    //
+    //}
+    //
+    //Цель этого задания: обратить ваше внимание на синтаксис
+    //объявления main() метода в языке программирования Java.
+}
---
+++ src/student_artemijs_talamajevs/homework/lesson_2_variables/homework/Turtle.java
@@ -0,0 +1,22 @@
+package student_artemijs_talamajevs.homework.lesson_2_variables.homework;
+
+public class Turtle {
+    public static void main(String[] args) {
+
+    }
+    //Дан код, нужно исправить синтаксическую ошибку,
+    //которая приводит к ошибке компиляции.
+    //Функционал класса писать не надо.
+    //Для сдачи работы создайте исправленный класс.
+    //
+    //class Turtle {
+    //
+    //	public static void main(String[] args)
+    //
+    //	}
+    //
+    //}
+    //
+    //Цель этого задания: обратить ваше внимание на синтаксис
+    //объявления метода в языке программирования Java.
+}
