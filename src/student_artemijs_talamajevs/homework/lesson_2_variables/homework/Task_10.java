7f36ae70e26127470e4e2d7a9072f89b9146054f
Artemij <artik8865@inbox.lv>
2022-05-07 19:20:08 +0300

    All in progress

---
+++ src/student_artemijs_talamajevs/homework/lesson_2_variables/homework/Task_10.java
@@ -0,0 +1,25 @@
+package student_artemijs_talamajevs.homework.lesson_2_variables.homework;
+
+import java.util.Scanner;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Circle radius");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * (radius * radius);
+        System.out.println("Circle area: " + area);
+        double circumference= Math.PI * 2*radius;
+        System.out.println("Circle perimeter: " + circumference) ;
+        //7,Напишите программу, которая запрашивает у пользователя
+        //радиус круга (вещественное число) и печатает на консоль
+        //периметр и площадь круга.
+        //
+        //Test Data:
+        //Radius = 7.5
+        //
+        //Expected Output
+        //Perimeter is = 47.12388980384689
+        //Area is = 176.71458676442586
+    }
+}
