e9a6609985e190fa67587f69ac1dc5fb8649a4e6
Artemij <artik8865@inbox.lv>
2022-05-08 18:20:06 +0300

    All in progress

---
+++ src/student_artemijs_talamajevs/homework/lesson_2_variables/homework/AttentionPlease.java
@@ -0,0 +1,21 @@
+package student_artemijs_talamajevs.homework.lesson_2_variables.homework;
+
+public class AttentionPlease {
+    //Заставьте эту программу работать!
+    //
+    //class AttentionPlease {
+    //
+    //    System.out.println("Code outside the method will not compile!");
+    //
+    //    public static void main(String[] args) {
+    //
+    //    }
+    //}
+    //
+    //Цель этого задания: обратить ваше внимания на то, где должен
+    //распологаться программный код. В языке программирования Java
+    //программный код с командами должен находиться внутри метода.
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
