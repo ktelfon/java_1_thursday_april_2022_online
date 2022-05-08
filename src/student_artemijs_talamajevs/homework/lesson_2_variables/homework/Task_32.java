9ea09ef757da8fff42dc28dccc576b61ec065573
Artemij <artik8865@inbox.lv>
2022-05-08 20:35:54 +0300

    All in progress

---
+++ src/student_artemijs_talamajevs/homework/lesson_2_variables/homework/Task_32.java
@@ -0,0 +1,13 @@
+package student_artemijs_talamajevs.homework.lesson_2_variables.homework;
+
+public class Task_32 {
+    //Напишите программу и продемонстрируйте в ней использование
+    //однострочного и многострочного комментариев.
+    public static void main(String[] args) {
+        //Single line comments
+        /*
+        *Multiline Comments
+        */
+
+    }
+}
