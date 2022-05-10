28a37358b8ae9a1304fad8b768c45faabca5846b
Igor0208 <igorperesunko994@gmail.com>
2022-05-10 22:04:58 +0300

    Homewokr Lesson2 Task 3,4,5,6,7 done

---
+++ src/student_igor_peresunko/homework/lesson2/level2/Task3.java
@@ -0,0 +1,20 @@
+package student_igor_peresunko.homework.lesson2.level2;
+// Дан код, нужно исправить синтаксическую ошибку,
+//    которая приводит к ошибке компиляции.
+//    Функционал класса писать не надо.
+//      Для сдачи работы создайте исправленный класс.
+
+// class Book {
+
+//  Цель этого задания: обратить ваше внимание на синтаксис
+//      объявления класса в языке программирования Java.
+
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/student_igor_peresunko/homework/lesson2/level2/Task4.java
@@ -0,0 +1,10 @@
+package student_igor_peresunko.homework.lesson2.level2;
+
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
+public class Task4 {
+    public static void main(String[] agrs){
+
+    }
+}
