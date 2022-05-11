7da507e0f8222ba632f229546ef3d09e3ec63b7d
olga <ol.grisa@gmail.com>
2022-05-11 15:58:56 +0300

    Homework Lesson2 Task9

---
+++ src/student_olga_suhusina/lesson2/homework/Task9.java
@@ -0,0 +1,39 @@
+package student_olga_suhusina.lesson2.homework;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args){
+        System.out.println("Hello! Input a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Expected Output: ");
+        int a1 = number*1;
+        System.out.println(a1);
+        int a2 = number*2;
+        System.out.println(a2);
+        int a3 = number*3;
+        System.out.println(a3);
+        int a4 = number*4;
+        System.out.println(a4);
+        int a5 = number*5;
+        System.out.println(a5);
+        int a6 = number*6;
+        System.out.println(a6);
+        int a7 = number*7;
+        System.out.println(a7);
+        int a8 = number*8;
+        System.out.println(a8);
+        int a9 = number*9;
+        System.out.println(a9);
+        int a10 = number*10;
+        System.out.println(a10);
+
+
+
+
+
+
+
+    }
+}
