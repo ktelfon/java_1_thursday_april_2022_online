2a4a1f616be3f6f1b1a7a2565b243d731e1b4b89
denissfjodorovs <ktelfon@gmail.com>
2022-05-11 08:30:31 +0300

    Removed approves from empty classes

--- src/student_igor_peresunko/homework/lesson2/level2/Task3.java
+++ src/student_igor_peresunko/homework/lesson2/level2/Task3.java
@@ -11,7 +11,6 @@ package student_igor_peresunko.homework.lesson2.level2;
 
 import teacher.codereview.CodeReview;
 
-@CodeReview(approved = true)
 public class Task3 {
     public static void main(String[] args) {
 
--- src/student_igor_peresunko/homework/lesson2/level2/Task6.java
+++ src/student_igor_peresunko/homework/lesson2/level2/Task6.java
@@ -1,8 +1,5 @@
 package student_igor_peresunko.homework.lesson2.level2;
 
-import teacher.codereview.CodeReview;
-
-@CodeReview(approved = true)
 public class Task6 {
     public static void main(String[] args){
 
