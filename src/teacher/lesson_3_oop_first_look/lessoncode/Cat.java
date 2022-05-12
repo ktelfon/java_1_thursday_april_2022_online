f4b346e8a6fb46e265032d5d6e4794f071aac215
denissfjodorovs <ktelfon@gmail.com>
2022-05-12 21:24:26 +0300

    Cat demo from lesson 3

---
+++ src/teacher/lesson_3_oop_first_look/lessoncode/Cat.java
@@ -0,0 +1,55 @@
+package teacher.lesson_3_oop_first_look.lessoncode;
+
+public class Cat {
+
+    private String name;
+    private int age;
+    private boolean hunger;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Cat(String name, int age, boolean hunger) {
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHunger(boolean hunger) {
+        this.hunger = hunger;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+
+        this.name = name;
+    }
+
+    public boolean isHunger(){
+        return hunger;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void makeNoise() {
+        System.out.println(name + ":Meow!");
+    }
+
+    public int makeOlder(int ageToAdd) {
+//        this.age = this.age + ageToAdd;
+        this.age += ageToAdd;
+        return this.age;
+    }
+
+}
