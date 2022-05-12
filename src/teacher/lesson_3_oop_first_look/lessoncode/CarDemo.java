362368022595bd20a446e4f0044bcc3aec47ba2b
denissfjodorovs <ktelfon@gmail.com>
2022-05-12 21:52:13 +0300

    Car demo from lesson 3

---
+++ src/teacher/lesson_3_oop_first_look/lessoncode/CarDemo.java
@@ -0,0 +1,13 @@
+package teacher.lesson_3_oop_first_look.lessoncode;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.changeBrand("AUDI");
+        System.out.println(myCar.getBrand());
+        System.out.println(myCar.getDistance());
+        myCar.drive(10);
+        System.out.println(myCar.getDistance());
+    }
+}
