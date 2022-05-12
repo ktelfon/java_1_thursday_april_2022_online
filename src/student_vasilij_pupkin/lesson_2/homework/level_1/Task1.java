b0fc62038c0943618201473544a7046d0117efcd
Deniss <ktelfon@gmail.com>
2022-05-12 10:04:05 +0300

    pupkin removed

--- src/student_vasilij_pupkin/lesson_2/homework/level_1/Task1.java
+++
@@ -1,7 +0,0 @@
-package student_vasilij_pupkin.lesson_2.homework.level_1;
-
-public class Task1 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/student_vasilij_pupkin/lesson_2/homework/level_1/Task2.java
+++
@@ -1,7 +0,0 @@
-package student_vasilij_pupkin.lesson_2.homework.level_1;
-
-public class Task2 {
-    public static void main(String[] args) {
-
-    }
-}
