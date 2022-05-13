c5235278d313e452e51e8ab7012cee0d87309beb
VladislavSolims <104584572+VladislavSolims@users.noreply.github.com>
2022-05-13 20:57:00 +0300

    level1 done

---
+++ src/student_Vladislavs_Solims/homework/lesson3/level1/Robot.java
@@ -0,0 +1,24 @@
+package student_Vladislavs_Solims.homework.lesson3.level1;
+
+public class Robot {
+    public Robot(){
+
+    }
+    private String hello;
+    private String name;
+    public void sayHello(String hello){
+        this.hello=hello;
+    }
+    public String getHello() {
+        return hello;
+    }
+    public void sayYourName(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
---
+++ src/student_Vladislavs_Solims/homework/lesson3/level3/Main.java
@@ -0,0 +1,4 @@
+package student_Vladislavs_Solims.homework.lesson3.level3;
+
+public class Main {
+}
