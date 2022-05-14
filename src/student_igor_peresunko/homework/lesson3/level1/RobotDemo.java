7a304f72fdcad18b602493039fb23ecb3fde225d
Igor0208 <igorperesunko994@gmail.com>
2022-05-14 17:25:27 +0300

    Homewokr Lesson2 Task 3,4,5,6,7 done

---
+++ src/student_igor_peresunko/homework/lesson3/level1/RobotDemo.java
@@ -0,0 +1,14 @@
+package student_igor_peresunko.homework.lesson3.level1;
+
+public class RobotDemo {
+    public static void main(String[] args){
+        Robot robot = new Robot();
+        robot.sayHello("Hello");
+        System.out.println(robot.getHello());
+        robot.sayYourName("my name is ROBO!");
+        System.out.println(robot.getName());
+    }
+
+
+
+}
