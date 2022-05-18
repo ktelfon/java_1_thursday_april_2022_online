f9057854ed4d041be164e01fafefaf4dbe89e0cc
Kristina <kristina_sh@yahoo.com>
2022-05-19 00:11:12 +0300

    completed tasks

---
+++ src/student_kristina_sapovalova/homework/lesson_3/level_2/Task7.java
@@ -0,0 +1,24 @@
+package student_kristina_sapovalova.homework.lesson_3.level_2;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        String myName = "Kristina";
+        System.out.println(myName);
+
+    }
+}
+
+/* Учимся работать со строками.
+
+Тема: создание переменной типа String.
+
+Напишите программу в которой создайте переменную
+типа String и присвойте ей в качестве начального значения
+ваше имя. Выведете на консоль значение этой переменной.
+
+Пример кода:
+
+    String myName = "Viktor";
+    System.out.println(myName); */
