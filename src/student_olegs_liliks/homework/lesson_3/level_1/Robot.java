8f4068539de1bb06e8dbdcb66017f17881d8ebba
olezhkou <olegsliliks@gmail.com>
2022-05-19 17:36:48 +0300

    My first commit.

---
+++ src/student_olegs_liliks/homework/lesson_3/level_1/Robot.java
@@ -0,0 +1,16 @@
+package student_olegs_liliks.homework.lesson_3.level_1;
+
+public class Robot {
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/student_olegs_liliks/homework/lesson_3/level_4/task_14/BookDemo.java
@@ -0,0 +1,9 @@
+package student_olegs_liliks.homework.lesson_3.level_4.task_14;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
