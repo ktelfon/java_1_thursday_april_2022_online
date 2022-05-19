ac4884eaffa115adc6a8527c2c91351a1bf824a2
Artemij <artik8865@inbox.lv>
2022-05-19 18:08:26 +0300

    lesson3_level_2

--- src/student_artemijs_talamajevs/lesson_3/level_2/Task_9.java
+++ src/student_artemijs_talamajevs/lesson_3/level_2/Task_9.java
@@ -1,7 +1,10 @@
 package student_artemijs_talamajevs.lesson_3.level_2;
 
+import teacher.codereview.CodeReview;
+
 import java.util.Scanner;
 
+@CodeReview(approved = true)
 public class Task_9 {
     public static void main(String[] args) {
         System.out.println("What is your name?");
