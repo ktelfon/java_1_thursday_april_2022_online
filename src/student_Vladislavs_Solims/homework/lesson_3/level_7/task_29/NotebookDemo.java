8d05e38404f9ba083fc036678430fd6803b56fcb
VladislavSolims <104584572+VladislavSolims@users.noreply.github.com>
2022-05-19 17:21:26 +0300

    now i'm done)

--- src/student_Vladislavs_Solims/homework/lesson_3/level_7/task_29/NotebookDemo.java
+++ src/student_Vladislavs_Solims/homework/lesson_3/level_7/task_29/NotebookDemo.java
@@ -1,13 +1,15 @@
 package student_Vladislavs_Solims.homework.lesson_3.level_7.task_29;
 
+import java.util.Scanner;
+
 public class NotebookDemo {
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
         Notebook notebook = new Notebook();
-        notebook.setModel("Asus");
+        notebook.setModel(scanner.next());
         System.out.println(notebook.getModel());
-        notebook.setDisplaySize(15);
+        notebook.setDisplaySize(scanner.nextInt());
         System.out.println(notebook.getDisplaySize());
         System.out.println(notebook.isCharged());
     }
