1a3a071c8a7c47baa943d7dc1b32322febe763b1
Kristina <kristina_sh@yahoo.com>
2022-05-19 17:16:16 +0300

    Lesson 3 Level 4 completed tasks

---
+++ src/student_kristina_sapovalova/homework/lesson_3/level_4/Book.java
@@ -0,0 +1,35 @@
+package student_kristina_sapovalova.homework.lesson_3.level_4;
+
+public class Book {
+
+    String title;
+
+    public Book(String bookTitle) {
+
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+
+        return this.title;
+    }
+}
+
+/* Task_14:
+Найти ошибку в коде.
+Исправить и запустить программу.
+Менять можно только класс Book.
+
+class Book {
+
+    String title
+
+    Book(String bookTitle) {
+        this.title = newTitle
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+} */
---
+++ src/student_kristina_sapovalova/homework/lesson_3/level_4/PhoneDemo.java
@@ -0,0 +1,11 @@
+package student_kristina_sapovalova.homework.lesson_3.level_4;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}
