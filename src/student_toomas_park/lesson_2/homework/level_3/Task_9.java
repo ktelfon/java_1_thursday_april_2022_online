e886b886701bf4cfdb3c91aaab1ad69e11048c8e
zapacher <toomas_park@mail.ru>
2022-05-22 01:56:21 +0300

    Done!

--- src/student_toomas_park/lesson_2/homework/level_3/Task_9.java
+++ src/student_toomas_park/lesson_2/homework/level_3/Task_9.java
@@ -1,4 +1,19 @@
 package student_toomas_park.lesson_2.homework.level_3;
-
+import java.util.Scanner;
 public class Task_9 {
+    public static void main(String[] args){
+        Scanner in1 = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number1 = in1.nextInt();
+        System.out.println(number1+" * 1 = "+number1);
+        System.out.println(number1+" * 2 = "+number1*2);
+        System.out.println(number1+" * 3 = "+number1*3);
+        System.out.println(number1+" * 4 = "+number1*4);
+        System.out.println(number1+" * 5 = "+number1*5);
+        System.out.println(number1+" * 6 = "+number1*6);
+        System.out.println(number1+" * 7 = "+number1*7);
+        System.out.println(number1+" * 8 = "+number1*8);
+        System.out.println(number1+" * 9 = "+number1*9);
+        System.out.println(number1+" * 10 = "+number1*10);
+    }
 }
