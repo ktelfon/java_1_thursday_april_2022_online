b09be3eda36611ffba13b0b40df72469da069942
zapacher <toomas_park@mail.ru>
2022-05-22 02:20:38 +0300

    Done!

--- src/student_toomas_park/lesson_1/homework/Task_12.java
+++ src/student_toomas_park/lesson_1/homework/Task_12.java
@@ -1,6 +1,6 @@
 package student_toomas_park.lesson_1.homework;
 
-public class Task12 {
+public class Task_12 {
     public static void main(String[] args) {
         float num1 = 50, num2 = 3, res;
         res = num1 * num2;
