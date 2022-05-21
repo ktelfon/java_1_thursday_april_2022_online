955e591b761c099ea73076143541e1ca231d94a9
zapacher <toomas_park@mail.ru>
2022-05-22 02:33:00 +0300

    Done!

--- src/student_toomas_park/lesson_2/homework/level_7/Task_28.java
+++ src/student_toomas_park/lesson_2/homework/level_7/Task_28.java
@@ -1,4 +1,12 @@
 package student_toomas_park.lesson_2.homework.level_7;
 
 public class Task_28 {
+    /*
+book                Book
+Bankaccount         BankAccount
+systemUnit          SysUnit
+Login_Service       LoginService
+banktransaction     BankTrasaction
+task1               Task_1
+     */
 }
