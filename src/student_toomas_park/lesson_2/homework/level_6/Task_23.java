64caa2d26f1afc43099edea886c06e7887e63107
zapacher <toomas_park@mail.ru>
2022-05-22 02:29:27 +0300

    renamed

--- src/student_toomas_park/lesson_2/homework/level_6/Task_23.java
+++ src/student_toomas_park/lesson_2/homework/level_6/Task_23.java
@@ -5,6 +5,7 @@ import teacher.codereview.CodeReview;
 @CodeReview(approved = true)
 public class Task_23 {
     public static void main(String[] args) {
+
         System.out.println("Code outside the method will not compile!");
     }
 }
