c2897cd849fdd42179319d3af35cef5bbb242181
zapacher <toomas_park@mail.ru>
2022-05-22 06:50:53 +0300

    Done!

---
+++ src/student_toomas_park/lesson_3/homework/level_1/Robot.java
@@ -0,0 +1,9 @@
+package student_toomas_park.lesson_3.homework.level_1;
+
+class Robot{
+    public Robot() {
+    }
+    public void sayHello() {
+        System.out.println("Hello!");
+        }
+    }
---
+++ src/student_toomas_park/lesson_3/homework/level_1/RobotDemo.java
@@ -0,0 +1,8 @@
+package student_toomas_park.lesson_3.homework.level_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+    }
+}
