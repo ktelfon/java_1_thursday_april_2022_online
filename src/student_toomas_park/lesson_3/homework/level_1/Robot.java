1dafc7ed7c0a12e178fcd7baa221f243cbccd479
zapacher <toomas_park@mail.ru>
2022-05-22 09:07:02 +0300

    Done!

--- src/student_toomas_park/lesson_3/homework/level_1/Robot.java
+++ src/student_toomas_park/lesson_3/homework/level_1/Robot.java
@@ -1,12 +1,16 @@
 package student_toomas_park.lesson_3.homework.level_1;
 
 class Robot{
-    public Robot() {
+    String name;
+    public Robot(String robotName) {
+        this.name = robotName;
     }
     public void sayHello() {
         System.out.println("Привет!");
     }
     public void sayName(){
-        System.out.println("Меня зовут R2D2");
+        System.out.println("Меня зовут " + this.name);
     }
+
+
 }
--- src/student_toomas_park/lesson_3/homework/level_1/RobotDemo.java
+++ src/student_toomas_park/lesson_3/homework/level_1/RobotDemo.java
@@ -2,8 +2,12 @@ package student_toomas_park.lesson_3.homework.level_1;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot("R2D2");
         robot1.sayHello();
         robot1.sayName();
+
+        Robot robot2 = new Robot("BoB");
+        robot2.sayHello();
+        robot2.sayName();
     }
 }
