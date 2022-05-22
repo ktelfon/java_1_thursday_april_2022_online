4465fbdc276d8743d99bacca209806a53b73e693
zapacher <toomas_park@mail.ru>
2022-05-22 09:12:32 +0300

    Done!

--- src/student_toomas_park/lesson_3/homework/level_4/BookDemo.java
+++ src/student_toomas_park/lesson_3/homework/level_4/BookDemo.java
@@ -1,8 +1,8 @@
 package student_toomas_park.lesson_3.homework.level_4;
 
-class BookDemo {
+public class BookDemo {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Book myBook = new Book("Principles");
         String bookTitle = myBook.getTitle();
         System.out.println("Book title = " + bookTitle);
