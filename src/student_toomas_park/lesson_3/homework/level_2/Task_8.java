52b836981085523a4145f806ae3b2a54a82dffd3
zapacher <toomas_park@mail.ru>
2022-05-22 07:01:10 +0300

    Done!

--- src/student_toomas_park/lesson_3/homework/level_2/Task_8.java
+++ src/student_toomas_park/lesson_3/homework/level_2/Task_8.java
@@ -1,4 +1,8 @@
 package student_toomas_park.lesson_3.homework.level_2;
 
 public class Task_8 {
+    public static void main(String[] args){
+        String myName = "Toomas";
+        System.out.println("Привет, меня зовут " + myName + " !");
+    }
 }
