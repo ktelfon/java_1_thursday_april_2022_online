6ce0d00bf49c8da707e5e0499d97028d0c541c7f
zapacher <toomas_park@mail.ru>
2022-05-22 09:18:58 +0300

    Done!

---
+++ src/student_toomas_park/lesson_3/homework/level_4/Car.java
@@ -0,0 +1,15 @@
+package student_toomas_park.lesson_3.homework.level_4;
+
+class Car {
+
+    String model;
+
+    public Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/student_toomas_park/lesson_3/homework/level_4/CarDemo.java
@@ -0,0 +1,11 @@
+package student_toomas_park.lesson_3.homework.level_4;
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
