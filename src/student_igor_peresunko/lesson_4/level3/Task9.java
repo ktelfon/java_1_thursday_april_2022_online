9bff66d466540caf5b0b9af581d26f907a70695b
Igor0208 <igorperesunko994@gmail.com>
2022-05-24 23:05:13 +0300

    Level3 . done

---
+++ src/student_igor_peresunko/lesson_4/level3/Task9.java
@@ -0,0 +1,28 @@
+package student_igor_peresunko.lesson_4.level3;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int number1 = input1.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter the second number: ");
+        int number2 = input2.nextInt();
+
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Enter the third number ");
+        int number3 = input3.nextInt();
+
+        if(number1<number2 && number2<number3){
+            System.out.println("increasing");
+        } else if (number1>number2 && number2>number3) {
+            System.out.println("decreasing");
+        }else {
+            System.out.println("Neither increasing or decreasing order");}
+
+
+    }
+}
