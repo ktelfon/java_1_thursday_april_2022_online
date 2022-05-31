0a7259409e5c1b1e7ec15a5400ad40b68e0c999b
Igor0208 <igorperesunko994@gmail.com>
2022-05-31 21:25:37 +0300

    Lesson 5 level 2 task 10

---
+++ src/student_igor_peresunko/homework/lesson_5/level2/Task10.java
@@ -0,0 +1,14 @@
+package student_igor_peresunko.homework.lesson_5.level2;
+
+public class Task10 {
+    public static void main(String[] args) {
+        int[] number = new int[3];
+        number[0] = 100;
+        number[1] = 200;
+        number[2] = 300;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(number[i]);
+        }
+
+    }
+}
