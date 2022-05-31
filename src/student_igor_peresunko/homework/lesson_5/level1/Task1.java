77c89e4cbfdc8547323b778f16476176d295d017
Igor0208 <igorperesunko994@gmail.com>
2022-05-31 22:12:27 +0300

    Lesson 5 level 1 task 1

---
+++ src/student_igor_peresunko/homework/lesson_5/level1/Task1.java
@@ -0,0 +1,17 @@
+package student_igor_peresunko.homework.lesson_5.level1;
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] number = new int[1];
+        number[0] = 10;
+        for (int i = 0; i < number.length; i++){
+            System.out.println(number[i]);
+
+
+
+
+        }
+
+    }
+}
