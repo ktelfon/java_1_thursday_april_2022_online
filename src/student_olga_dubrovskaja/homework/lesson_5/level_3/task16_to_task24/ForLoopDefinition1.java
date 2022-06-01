6efcb4edfc9cb691d2b7d5b942d7a7187d97ca03
Milrinone <dubrovskaja.olga@gmail.com>
2022-06-01 19:48:07 +0300

    lesson5 homework

---
+++ src/student_olga_dubrovskaja/homework/lesson_5/level_3/task16_to_task24/ForLoopDefinition1.java
@@ -0,0 +1,95 @@
+package student_olga_dubrovskaja.homework.lesson_5.level_3.task16_to_task24;
+
+// Task 16
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 17
+class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 18
+class ForLoopDefinition3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 19
+
+class ForLoopDefinition4 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 20
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 21
+
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+}
+
+// Task 22
+
+class ForLoopDefinition7 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 23
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Task 24
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
