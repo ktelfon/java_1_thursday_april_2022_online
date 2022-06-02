4c45c61f6fd201ec2ded8f7fbd8dac96a0f4980d
denissfjodorovs <ktelfon@gmail.com>
2022-06-02 21:53:22 +0300

    Vet clinic part 2

--- src/teacher/lesson_5_arrays_for_loop/lessoncode/VetClinicDemo.java
+++ src/teacher/lesson_5_arrays_for_loop/lessoncode/VetClinicDemo.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class VetClinicDemo {
 
-    final static String MENU = " Enter : (register(1)) (show_patients(2)) (quit(3))";
+    final static String MENU = " Enter : (register(1)) (show_patients(2)) (discharge(3)) (quit(4))";
 
     public static void main(String[] args) {
         VetClinic clinic = new VetClinic();
@@ -16,7 +16,7 @@ public class VetClinicDemo {
         System.out.println(MENU);
         Scanner scr = new Scanner(System.in);
         int input = scr.nextInt();
-        while (3 != input) {
+        while (4 != input) {
             switch (input) {
                 case 1:
                     System.out.println("Enter pets name:");
@@ -27,6 +27,11 @@ public class VetClinicDemo {
                     break;
                 case 2:
                     clinic.showPatients();
+                    break;
+                case 3:
+                    System.out.println("Enter pets name:");
+                    String catNameToDischarge = scr.next();
+                    clinic.dischargeCat(catNameToDischarge);
                     break;
                 default:
                     System.out.println("Unknown command.");
