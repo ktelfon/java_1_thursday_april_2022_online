9baea75da5d362263e60bdd49e3d6d7f436c0c10
Artemij <artik8865@inbox.lv>
2022-06-02 18:31:39 +0300

    lesson5_level_3

---
+++ src/student_artemijs_talamajevs/lesson_5/level_3/ForLoopDefinition4.java
@@ -0,0 +1,10 @@
+package student_artemijs_talamajevs.lesson_5.level_3;
+
+public class ForLoopDefinition4 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
