1a927aec65d027898c32b2414bb65d42b8b7975a
olezhkou <olegsliliks@gmail.com>
2022-06-02 19:15:49 +0300

    My first commit.

--- src/student_olegs_liliks/homework/lesson_5/level_4/task_26.java
+++ src/student_olegs_liliks/homework/lesson_5/level_4/task_26.java
@@ -1,23 +1,19 @@
 package student_olegs_liliks.homework.lesson_5.level_4;
 
-import teacher.codereview.CodeReview;
-import teacher.codereview.CodeReviewComment;
-
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
-@CodeReview(approved = false)
-@CodeReviewComment(teacher = "Class name must start with a capital letter.")
-public class task_25 {
+public class task_26 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write array length: ");
         int arrayLength = scanner.nextInt();
         int [] numbers = new int[arrayLength];
+        Random random = new Random();
         for (int i = 0; i<arrayLength; i++){
-        System.out.print("Write numbers : ");
-        numbers[i] = scanner.nextInt();}
-        System.out.println("Numbers are : " + Arrays.toString(numbers));
+            System.out.println("Numbers are : " + random.nextInt());}
 
     }
 }
+
