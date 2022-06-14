88384f6ef53035472bff4617a8bacdf1677f1ecd
Milrinone <dubrovskaja.olga@gmail.com>
2022-06-14 20:31:48 +0300

    lesson7 homework vol.2 - credit card tests added

--- src/student_olga_dubrovskaja/homework/lesson_7/level_6/task_10/CreditCard.java
+++ src/student_olga_dubrovskaja/homework/lesson_7/level_6/task_10/CreditCard.java
@@ -3,23 +3,24 @@ package student_olga_dubrovskaja.homework.lesson_7.level_6.task_10;
 import java.util.Scanner;
 
 class CreditCard {
-    private String cardNumber;
-    private int pin;
+
+    private final String cardNumber;
+    private final int pin;
     private float balance;
     private float creditLimit;
     private float debt;
 
-    public CreditCard(String cardNumber, int pin){
+    public CreditCard(String cardNumber, int pin) {
         this.pin = pin;
         this.cardNumber = cardNumber;
         this.balance = 0f;
         this.debt = 0f;
     }
 
-    public void deposit(){
-        if(checkPin(getPinFromUser())){
+    public void deposit() {
+        if (checkPin(getPinFromUser())) {
             float amountToDeposit = getMoneyAmountFromUser("deposit");
-            if(checkCreditDeposit()){
+            if (checkCreditDeposit()) {
                 payDebt(amountToDeposit);
             } else {
                 addToBalance(amountToDeposit);
@@ -29,10 +30,22 @@ class CreditCard {
         }
     }
 
-    public void withdraw(){
-        if(checkPin(getPinFromUser())){
+    public void deposit(int pin, float amount) {
+        if (checkPin(pin)) {
+            if (checkCreditDeposit()) {
+                payDebt(amount);
+            } else {
+                addToBalance(amount);
+            }
+        } else {
+            operationDeclined("wrong pin");
+        }
+    }
+
+    public void withdraw() {
+        if (checkPin(getPinFromUser())) {
             float amountToWithdraw = getMoneyAmountFromUser("withdraw");
-            if (checkBalanceWithdraw(amountToWithdraw)){
+            if (checkBalanceWithdraw(amountToWithdraw)) {
                 getMoneyFromBalance(amountToWithdraw);
             } else if (checkCreditWithdraw(amountToWithdraw)) {
                 getMoneyFromCredit(amountToWithdraw);
@@ -43,42 +56,57 @@ class CreditCard {
             operationDeclined("wrong pin");
         }
     }
-    private int getPinFromUser(){
+
+    public void withdraw(int pin, float amount) {
+        if (checkPin(pin)) {
+            if (checkBalanceWithdraw(amount)) {
+                getMoneyFromBalance(amount);
+            } else if (checkCreditWithdraw(amount)) {
+                getMoneyFromCredit(amount);
+            } else {
+                operationDeclined("Not enough money");
+            }
+        } else {
+            operationDeclined("wrong pin");
+        }
+    }
+
+    private int getPinFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter pin:");
         return scanner.nextInt();
     }
 
-    private boolean checkPin(int userPin){
+    private boolean checkPin(int userPin) {
         return this.pin == userPin;
     }
 
-    private void operationDeclined(String reason){
+    private void operationDeclined(String reason) {
         System.out.println("Operation declined: " + reason);
     }
 
-    private boolean checkBalanceWithdraw(float amountFromUser){
+    private boolean checkBalanceWithdraw(float amountFromUser) {
         return amountFromUser <= this.balance;
     }
 
-    private boolean checkCreditWithdraw(float amount){
-        return this.creditLimit - this.debt <= (this.balance - amount) ;
+    private boolean checkCreditWithdraw(float amount) {
+        return this.creditLimit - this.debt <= (this.balance - amount);
     }
 
-    private void getMoneyFromBalance(float amount){
+    private void getMoneyFromBalance(float amount) {
         this.balance = this.balance - amount;
     }
 
-    private void getMoneyFromCredit(float amount){
-            this.debt = (amount - this.balance) * -1;
-            this.balance = 0;
+    private void getMoneyFromCredit(float amount) {
+        this.debt = (amount - this.balance) * -1;
+        this.balance = 0;
     }
 
-    private boolean checkCreditDeposit(){
+    private boolean checkCreditDeposit() {
         return this.debt < 0;
     }
 
-    private void payDebt(float amount){
+    private void payDebt(float amount) {
         if (this.debt + amount <= 0) {
             this.debt = this.debt + amount;
         } else {
@@ -86,23 +114,48 @@ class CreditCard {
             this.debt = 0;
         }
     }
-    private void addToBalance(float amount){
+
+    private void addToBalance(float amount) {
         this.balance += amount;
     }
-    private float getMoneyAmountFromUser(String operationName){
+
+    private float getMoneyAmountFromUser(String operationName) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter amount to " + operationName);
-        return scanner.nextFloat();
+        if (scanner.hasNextFloat()) {
+            float moneyAmount = scanner.nextFloat();
+            if (moneyAmount > 0) {
+                return moneyAmount;
+            } else {
+                operationDeclined("Invalid amount");
+                return 0;
+            }
+        } else {
+            operationDeclined("Invalid amount");
+        }
+        return 0;
     }
 
     public void setCreditLimit(float creditLimit) {
         this.creditLimit = creditLimit * -1;
     }
 
-    public void printCreditCardInfo(){
+    public void printCreditCardInfo() {
         System.out.println("Credit card nr: " + this.cardNumber);
         System.out.println("Balance: " + this.balance);
         System.out.println("Credit debt: " + debt);
         System.out.println("Credit limit: " + creditLimit);
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public float getCreditLimit() {
+        return creditLimit;
+    }
+
+    public float getDebt() {
+        return debt;
     }
 }
--- src/student_olga_dubrovskaja/homework/lesson_7/level_6/task_10/CreditCardTest.java
+++ src/student_olga_dubrovskaja/homework/lesson_7/level_6/task_10/CreditCardTest.java
@@ -1,2 +1,64 @@
-package student_olga_dubrovskaja.homework.lesson_7.level_6.task_10;public class CreditCardTest {
+package student_olga_dubrovskaja.homework.lesson_7.level_6.task_10;
+
+class CreditCardTest {
+    public static void main(String[] args) {
+        CreditCardTest test = new CreditCardTest();
+        test.testDeposit();
+        test.testWithdrawCredit();
+        test.withdrawFromBalanceAndCredit();
+        test.depositToBalanceAndCredit();
+        test.testWrongPin();
+
+    }
+
+    public void testDeposit() {
+        CreditCard card = new CreditCard("1111 1111 1111 1111", 1111);
+        card.deposit(1111, 1000);
+        float expectedResult = 1000;
+        checkResult(expectedResult == card.getBalance(), "test simple deposit");
+    }
+
+    public void testWithdrawCredit() {
+        CreditCard card = new CreditCard("1111 1111 1111 1111", 1111);
+        card.setCreditLimit(1000);
+        card.withdraw(1111, 100);
+        float expectedResult = -100;
+        checkResult(expectedResult == card.getDebt(), "test withdraw from credit");
+    }
+
+    public void withdrawFromBalanceAndCredit() {
+        CreditCard card = new CreditCard("1111 1111 1111 1111", 1111);
+        card.setCreditLimit(1000);
+        card.deposit(1111, 50);
+        card.withdraw(1111, 100);
+        float expectedResult = -50;
+        checkResult(expectedResult == card.getDebt(), "test withdraw from balance and credit");
+    }
+
+    public void depositToBalanceAndCredit() {
+        CreditCard card = new CreditCard("1111 1111 1111 1111", 1111);
+        card.setCreditLimit(1000);
+        card.withdraw(1111, 50);
+        card.deposit(1111, 100);
+        float expectedResult = 50;
+        checkResult(expectedResult == card.getBalance(), "test deposit to balance and credit");
+    }
+
+
+    public void testWrongPin() {
+        CreditCard card = new CreditCard("1111 1111 1111 1111", 1111);
+        card.deposit(2222, 1000);
+        float expectedResult = 0;
+        checkResult(expectedResult == card.getBalance(), "test wrong pin");
+    }
+
+
+    public void checkResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " OK");
+        } else {
+            System.out.println(testName + " FAIL");
+        }
+    }
+
 }
