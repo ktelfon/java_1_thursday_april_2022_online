df333bd447b5343628b5fc14c39d4b45d2060ff8
denissfjodorovs <ktelfon@gmail.com>
2022-07-07 22:00:18 +0300

    some pagination stuff

--- src/student_olga_dubrovskaja/homework/lesson_12/level6/task30_to_task38/SearchCriteria.java
+++ src/student_olga_dubrovskaja/homework/lesson_12/level6/task30_to_task38/SearchCriteria.java
@@ -1,5 +1,9 @@
 package student_olga_dubrovskaja.homework.lesson_12.level6.task30_to_task38;
 
 public interface SearchCriteria {
+
+    Page page = new Page(0,0);
     boolean match(Book book);
+
+
 }
