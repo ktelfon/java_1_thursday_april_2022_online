5d2ba4b79f9fe1668ab832384005f1f685055d7c
zapacher <toomas_park@mail.ru>
2022-08-01 16:45:41 +0300

    /

---
+++ src/student_toomas_park/lesson_5/homework/level_2/Task_15.java
@@ -0,0 +1,12 @@
+package student_toomas_park.lesson_5.homework.level_2;
+
+    public class Task_15 {public static void main(String[] args) {
+        double[] numbers = {-3,2.6,550};
+        System.out.println("Число 1 " + numbers[0]);
+        System.out.println("Число 2 " + numbers[1]);
+        System.out.println("Число 3 " + numbers[2]);
+        System.out.println("Число 1 увеличено на 2 " + numbers[0]+2);
+        System.out.println("Число 2 увеличено на 2 " + numbers[1]+2);
+        System.out.println("Число 3 увеличено на 2 " + numbers[2]+2);
+    }
+}
