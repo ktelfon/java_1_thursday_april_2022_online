a27090684345e18c6114628ec5be5995a4ed50ab
zapacher <toomas_park@mail.ru>
2022-08-01 16:52:28 +0300

    /

---
+++ src/student_toomas_park/lesson_5/homework/level_3/Task_21.java
@@ -0,0 +1,8 @@
+package student_toomas_park.lesson_5.homework.level_3;
+
+public class Task_21 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+}
