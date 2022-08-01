977e0aa2875d962712a1332adb0f56cab6843c04
zapacher <toomas_park@mail.ru>
2022-08-01 16:11:32 +0300

    /

---
+++ src/student_toomas_park/lesson_5/homework/level_1/Task_5.java
@@ -0,0 +1,7 @@
+package student_toomas_park.lesson_5.homework.level_1;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+}
---
+++ src/student_toomas_park/lesson_5/homework/level_1/Task_6.java
@@ -0,0 +1,7 @@
+package student_toomas_park.lesson_5.homework.level_1;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, };
+    }
+}
