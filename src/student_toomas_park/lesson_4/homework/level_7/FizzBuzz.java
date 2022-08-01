d2821ba2a64e83982921d3cbea826b46b1202e81
zapacher <toomas_park@mail.ru>
2022-08-01 15:02:17 +0300

    /

--- src/student_toomas_park/lesson_4/homework/level_7/FizzBuzz.java
+++ src/student_toomas_park/lesson_4/homework/level_7/FizzBuzz.java
@@ -2,25 +2,14 @@ package student_toomas_park.lesson_4.homework.level_7;
 
 import java.util.Scanner;
 
-public class FizzBuzz {
+class FizzBuzz {
     public String detect(int number) {
-        Scanner input = new Scanner(System.in);
-        int number1 = input.nextInt();
-        if (number1 / 3 != 0) {
-            if (number1 / 5 != 0) {
-                return "FizzBuzz";
-            } else {
-                return "Fizz";
-            }
-        } else if (number1 / 5 != 0) {
-            if (number / 3 != 0) {
-                return "FizzBuzz";
-            } else {
-                return "Buzz";
-            }
-        } else {
-            System.out.println("" + number1);
-        }
-        return "";
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else return "" + number;
     }
 }
--- src/student_toomas_park/lesson_4/homework/level_7/FizzBuzzTest.java
+++ src/student_toomas_park/lesson_4/homework/level_7/FizzBuzzTest.java
@@ -1,23 +1,41 @@
 package student_toomas_park.lesson_4.homework.level_7;
 
 import student_toomas_park.lesson_4.homework.level_7.FizzBuzz;
-import teacher.lesson_4_if_statement.homework.level_7_senior.solutions.super_task_4.LightColorDetector;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
     public static void main(String[] args) {
-        FizzBuzzTest FizzBuzzTest = new FizzBuzzTest();
-        FizzBuzzTest.detect();
+        FizzBuzzTest test = new FizzBuzzTest();
+        test.testn1(3);
+        test.testn2(5);
+        test.testn3(15);
+        test.testn4(4);
     }
-
-    public void detect() {
-        int number1 = 579;
-        String expectedResult = "Green";
-        FizzBuzz FizzBuzz = new FizzBuzz();
-        String realResult = FizzBuzz.detect(number1);
-        if (realResult == expectedResult) {
-            System.out.println("Light test = OK! Expect : " + expectedResult + " Real :  " + realResult);
-        } else {
-            System.out.println("even test = FAIL! Expect : " + expectedResult + " , Real :  " + realResult);
+        public void testn1(int number){
+            String expectedResult = "Fizz";
+            FizzBuzz testNumber = new FizzBuzz();
+            tResult(expectedResult.equals(testNumber.detect(number)), "test 3 ");
         }
-    }
+        public void testn2(int number){
+            String expectedResult = "Buzz";
+            FizzBuzz testNumber = new FizzBuzz();
+            tResult(expectedResult.equals(testNumber.detect(number)), "test 5 ");
+        }
+        public void testn3(int number){
+            String expectedResult = "FizzBuzz";
+            FizzBuzz testNumber = new FizzBuzz();
+            tResult(expectedResult.equals(testNumber.detect(number)), "test 15 ");
+        }
+        public void testn4(int number){
+            String expectedResult = "" +number;
+            FizzBuzz testNumber = new FizzBuzz();
+            tResult(expectedResult.equals(testNumber.detect(number)), "test 4 ");
+        }
+
+        public void tResult(boolean result, String test) {
+            if (result) {
+                System.out.println(test + "OK");
+            } else {
+                System.out.println(test + "FAIL");
+            }
+        }
 }
