b364f8f47d7f0fbc225a9f03ccf50d49c7cf12b5
zapacher <toomas_park@mail.ru>
2022-08-01 16:14:42 +0300

    /

---
+++ src/student_toomas_park/lesson_5/homework/level_1/Task_8.java
@@ -0,0 +1,11 @@
+package student_toomas_park.lesson_5.homework.level_1;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+
+}
