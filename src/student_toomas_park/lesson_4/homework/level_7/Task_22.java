aab86bfc373c56b70ee2aa009340c8cd588dd840
zapacher <toomas_park@mail.ru>
2022-08-01 13:53:45 +0300

    /

--- src/student_toomas_park/lesson_4/homework/level_7/Task_22.java
+++ src/student_toomas_park/lesson_4/homework/level_7/Task_22.java
@@ -3,23 +3,24 @@ package student_toomas_park.lesson_4.homework.level_7;
 import java.util.Scanner;
 
 public class Task_22 {
-    public String main(int number){
+    public String main(int number) {
         Scanner input = new Scanner(System.in);
         int number1 = input.nextInt();
-        if (number1/3!=0){
-            if(number1/5!=0) {
+        if (number1 / 3 != 0) {
+            if (number1 / 5 != 0) {
                 System.out.println("FizzBuzz");
-            }else {
+            } else {
                 System.out.println("Fizz");
             }
-        } else if (number1/5!=0) {
-            if(number/3!=0) {
+        } else if (number1 / 5 != 0) {
+            if (number / 3 != 0) {
                 System.out.println("FizzBuzz");
-            }else {
+            } else {
                 System.out.println("Buzz");
             }
-        }else {
-            return "" = number1;
+        } else {
+            System.out.println("" + number1);
         }
+        return "";
     }
 }
