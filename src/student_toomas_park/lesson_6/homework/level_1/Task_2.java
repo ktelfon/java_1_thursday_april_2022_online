d369b123585ffa8d6d1be70edd19fc7e247ca0ac
zapacher <toomas_park@mail.ru>
2022-08-02 10:16:54 +0300

    /

---
+++ src/student_toomas_park/lesson_6/homework/level_1/Task_2.java
@@ -0,0 +1,11 @@
+package student_toomas_park.lesson_6.homework.level_1;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+            System.out.println(i);
+        i++;
+    }
+}
+}
---
+++ src/student_toomas_park/lesson_6/homework/level_1/Task_6.java
@@ -0,0 +1,7 @@
+package student_toomas_park.lesson_6.homework.level_1;
+
+public class Task_6 {
+    public boolean isEven(int number) {
+        if (isEven(number)){return true;}return false;
+    }
+}
