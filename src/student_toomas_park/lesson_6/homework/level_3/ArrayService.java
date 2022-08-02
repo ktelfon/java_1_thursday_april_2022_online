4d476f2dc2818cd96c404826fce0fd4b7c6d50ad
zapacher <toomas_park@mail.ru>
2022-08-02 11:46:43 +0300

    /

---
+++ src/student_toomas_park/lesson_6/homework/level_3/ArrayService.java
@@ -0,0 +1,36 @@
+package student_toomas_park.lesson_6.homework.level_3;
+
+public class ArrayService {
+
+    public boolean contains(int[] numbers, int numberToSearch) {
+        boolean contains = false;
+        for (int i : numbers) {
+            if (i == numberToSearch) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
+
+    public int count(int[] numbers, int numberToSearch) {
+        int count = 0;
+        for (int i : numbers) {
+            if (i == numberToSearch) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public boolean replaceFirst(int[] numbers, int numberToReplace, int newNumber) {
+        boolean replace = false;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == numberToReplace) {
+                numbers[i] = newNumber;
+                replace = true;
+                break;
+            }
+        }
+        return replace;
+    }
+}
---
+++ src/student_toomas_park/lesson_6/homework/level_3/ArrayServiceTest.java
@@ -0,0 +1,70 @@
+package student_toomas_park.lesson_6.homework.level_3;
+
+import java.util.Arrays;
+
+public class ArrayServiceTest {
+    public static void main(String[] args) {
+        ArrayServiceTest test = new ArrayServiceTest();
+        test.shouldContainNumber();
+        test.shouldNotContainNumber();
+        test.shouldCount();
+        test.shouldCountZero();
+        test.shouldReplaceFirstNumber();
+        test.shouldNotReplaceFirstNumber();
+    }
+    public static void shouldContainNumber(){
+        ArrayService testArrayService = new ArrayService();
+        int[] testArray = {5, 4, 3, 2, 1};
+        int testNumber = 3;
+        boolean expectResult = true;
+        checkTestResult(testArrayService.contains(testArray, testNumber) == expectResult, "Test contains number " + testNumber + " ");
+    }
+    public static void shouldNotContainNumber(){
+        ArrayService testArrayService = new ArrayService();
+        int[] testArray = {5, 4, 3, 2, 1};
+        int testNumber = 6;
+        boolean expectResult = false;
+        checkTestResult(testArrayService.contains(testArray, testNumber) == expectResult, "Test NOT contains number " + testNumber + " ");
+    }
+    public static void shouldCount(){
+        ArrayService testArrayService = new ArrayService();
+        int[] testArray = {5, 4, 4, 4, 1};
+        int testNumber = 4;
+        int expectResult = 3;
+        checkTestResult(testArrayService.count(testArray, testNumber) == expectResult, "Test count must be 3 : ");
+    }
+    public static void shouldCountZero(){
+        ArrayService testArrayService = new ArrayService();
+        int[] testArray = {5, 4, 4, 4, 1};
+        int testNumber = 2;
+        int expectResult = 0;
+        checkTestResult(testArrayService.count(testArray, testNumber) == expectResult, "Test count must be 0 : ");
+    }
+
+    public static void shouldReplaceFirstNumber(){
+       ArrayService testArrayService = new ArrayService();
+        int[] numbers = {5, 3, 3, 2, 1};
+        int numberToReplace = 3;
+        int newNumber = 8;
+        int[] expectResult = {5, 8, 3, 2, 1};
+        testArrayService.replaceFirst(numbers, numberToReplace, newNumber);
+        checkTestResult(Arrays.equals(numbers, expectResult), "Test repalced first number " + numberToReplace + " with " + newNumber + " : ");
+    }
+    public static void shouldNotReplaceFirstNumber(){
+        ArrayService testArrayService = new ArrayService();
+        int[] numbers = {5, 3, 3, 2, 1};
+        int numberToReplace = 13;
+        int newNumber = 8;
+        int[] expectResult = {5, 3, 3, 2, 1};
+        testArrayService.replaceFirst(numbers, numberToReplace, newNumber);
+        checkTestResult(Arrays.equals(numbers, expectResult), "Test NOT repalce first number " + numberToReplace + " with " + newNumber + " : ");
+    }
+
+    private static void checkTestResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " True ");
+        } else {
+            System.out.println(testName + " False ");
+        }
+    }
+}
