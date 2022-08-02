e927531198afec2087c2cb6b4ba0c6d74b7b8f5d
zapacher <toomas_park@mail.ru>
2022-08-02 10:31:39 +0300

    /

---
+++ src/student_toomas_park/lesson_6/homework/level_2/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,11 @@
+package student_toomas_park.lesson_6.homework.level_2;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+    }
+}
---
+++ src/student_toomas_park/lesson_6/homework/level_2/DayOfTheWeekDetectorTest.java
@@ -0,0 +1,89 @@
+package student_toomas_park.lesson_6.homework.level_2;
+
+
+class DayOfTheWeekDetectorTest {
+    public void testMonday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 1;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Monday test:");
+        compareResult("Monday", testResult);
+    }
+
+    public void testTuesday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 2;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Tuesday test:");
+        compareResult("Tuesday", testResult);
+    }
+
+    public void testWednesday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 3;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Wednesday test:");
+        compareResult("Wednesday", testResult);
+    }
+
+    public void testThursday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 4;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Thursday test:");
+        compareResult("Thursday", testResult);
+    }
+
+    public void testFriday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 5;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Friday test:");
+        compareResult("Friday", testResult);
+    }
+
+    public void testSaturday() {
+       DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 6;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Saturday test:");
+        compareResult("Saturday", testResult);
+    }
+
+    public void testSunday() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 7;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Sunday test:");
+        compareResult("Sunday", testResult);
+    }
+
+    public void testWrongDay() {
+        DayOfTheWeekDetector testWeek = new DayOfTheWeekDetector();
+        int dayNumber = 8;
+        String testResult = testWeek.findDayOfTheWeek(dayNumber);
+        System.out.print("Wrong day test:");
+        compareResult("Not correct day number", testResult);
+    }
+
+    public void compareResult(String expected, String test) {
+        if (expected.equals(test)) {
+            System.out.println(" True ");
+        } else {
+            System.out.println(" False");
+        }
+    }
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetectorTest test = new DayOfTheWeekDetectorTest();
+        test.testMonday();
+        test.testTuesday();
+        test.testWednesday();
+        test.testThursday();
+        test.testFriday();
+        test.testSaturday();
+        test.testSunday();
+        test.testWrongDay();
+
+    }
+}
