a80c0c98d3a4d137741b8320c4adbd0737c1bf69
zapacher <toomas_park@mail.ru>
2022-09-03 16:16:56 +0300

    0/

---
+++ src/student_toomas_park/lesson_8/homework/level_1/FraudDetector.java
@@ -0,0 +1,9 @@
+package student_toomas_park.lesson_8.homework.level_1;
+
+class FraudDetector {
+
+    boolean isFraud(Transaction t) {
+        return true;
+    }
+
+}
---
+++ src/student_toomas_park/lesson_8/homework/level_1/Trader.java
@@ -0,0 +1,6 @@
+package student_toomas_park.lesson_8.homework.level_1;
+
+public class Trader {
+    private String fullName;
+    private String city;
+}
