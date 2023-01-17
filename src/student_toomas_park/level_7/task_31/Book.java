76e34a6a8edb8d5e9b97145a39656dd49a381b1c
Toomas Park <toomas.park@bigwintek.com>
2023-01-17 13:23:02 +0200

    Done

---
+++ src/student_toomas_park/level_7/task_31/Book.java
@@ -0,0 +1,63 @@
+package student_toomas_park.level_7.task_31;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Book {
+    private String title;
+    private String ticket;
+    private Date returnDate;
+    private boolean reserved;
+    private User user;
+
+    public Book(String title, String ticket, Date returnDate, boolean reserved, User user) {
+        this.title = title;
+        this.ticket = ticket;
+        this.returnDate = returnDate;
+        this.reserved = reserved;
+        this.user = user;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isReserved() {
+        return reserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        this.reserved = reserved;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/student_toomas_park/level_7/task_31/User.java
@@ -0,0 +1,30 @@
+package student_toomas_park.level_7.task_31;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
