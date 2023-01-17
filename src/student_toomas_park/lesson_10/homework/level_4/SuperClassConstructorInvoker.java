aa1e785b7f060814067e6dd589460c920ab29a3d
Toomas Park <toomas.park@bigwintek.com>
2023-01-17 14:12:24 +0200

    Done

---
+++ src/student_toomas_park/lesson_10/homework/level_4/SuperClassConstructorInvoker.java
@@ -0,0 +1,8 @@
+package student_toomas_park.lesson_10.homework.level_4;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
